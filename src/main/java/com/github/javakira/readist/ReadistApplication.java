923c975f75bd2571144e793cbf3d77e1b7e4c434
JavaKira <JavaKira@yandex.ru>
2022-06-05 17:08:46 +0300

    init commit

---
+++ src/main/java/com/github/javakira/readist/ReadistApplication.java
@@ -0,0 +1,13 @@
+package com.github.javakira.readist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/javakira/readist/ReadistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.javakira.readist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

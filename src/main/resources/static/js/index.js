caca92f834fa73596be8ba4eeb824eb325dc2dd7
JavaKira <JavaKira@yandex.ru>
2022-06-17 16:10:51 +0300

    Added index.html new session add form

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+    const addButton = document.getElementById(`AddButton`);
+    const addPanel = document.getElementById(`AddPanel`);
+    addPanel.hidden = true;
+    const bookNameText = document.getElementById(`BookNameText`);
+    const expositionText = document.getElementById(`ExpositionText`);
+    const pagesCounter = document.getElementById(`PagesCounter`);
+    const addPanelSubmitButton = document.getElementById(`AddPanelSubmitButton`);
+
+    addButton.onclick = () => {
+        addPanel.hidden = false;
+    }
+
+    addPanelSubmitButton.onclick = () => {
+        let request = new XMLHttpRequest();
+        request.open("POST", "/add");
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(JSON.stringify({
+            date: new Date,
+            bookName: bookNameText.value,
+            pagesCount: parseInt(pagesCounter.value),
+            expositionText: expositionText.value,
+        }))
+    }
+}
